35979a9d6cab51f07281da71166194669a4f10d8
Prabhjot Singh <prabhkejotsingh@gmail.com>
2020-12-22 18:00:26 -0500

    Change ORACLE SQL to PostgreSQL

--- src/main/java/com/mcit/pm/config/HibernateConfig.java
+++ src/main/java/com/mcit/pm/config/HibernateConfig.java
@@ -1,4 +1,3 @@
-
 package com.mcit.pm.config;
 
 import java.sql.SQLException;
@@ -10,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
@@ -19,42 +19,53 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ComponentScan({"com.mcit"})
 public class HibernateConfig {
-        @Bean
+
+    @Bean
     public LocalSessionFactoryBean sessionFactory() throws SQLException {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSourceOracle());
+//        sessionFactory.setDataSource(dataSourceOracle());
+        sessionFactory.setDataSource(getDataSource());
         sessionFactory.setPackagesToScan("com.mcit.pm.model.entities");
-        sessionFactory.setHibernateProperties(hibernateProperties()); 
+        sessionFactory.setHibernateProperties(hibernateProperties());
         return sessionFactory;
     }
-    
+
     @Bean
     @Autowired
     public HibernateTransactionManager transactionManager(SessionFactory s) {
-       HibernateTransactionManager txManager = new HibernateTransactionManager();
-       txManager.setSessionFactory(s);
-       return txManager;
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(s);
+        return txManager;
     }
-    
+
+//    @Bean
+//    @Autowired
+//    public DataSource dataSourceOracle() throws SQLException {
+//        OracleDataSource dataSource = new OracleDataSource();
+//        dataSource.setURL("jdbc:oracle:thin:@localhost:1521:XE");
+//        dataSource.setUser("admin_pm");
+//        dataSource.setPassword("admin");
+//        return dataSource;
+//    }
+
     @Bean
-    @Autowired
-    public DataSource dataSourceOracle() throws SQLException {
-        OracleDataSource dataSource = new OracleDataSource();
-        dataSource.setURL("jdbc:oracle:thin:@localhost:1521:XE");
-        dataSource.setUser("admin_pm");
-        dataSource.setPassword("admin");
-        return dataSource;
+    public DriverManagerDataSource getDataSource() {
+
+        DriverManagerDataSource bds = new DriverManagerDataSource();
+        bds.setDriverClassName("org.postgresql.Driver");
+        bds.setUrl("jdbc:postgresql://localhost:5432/admin_pm");
+        bds.setUsername("postgres");
+        bds.setPassword("admin");
+        return bds;
     }
-    
 
     private final Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
         hibernateProperties.setProperty(
-          "hibernate.dialect", "org.hibernate.dialect.OracleDialect");
-        hibernateProperties.setProperty("hibernate.show_sql", "true"); 
+                "hibernate.dialect", "org.hibernate.dialect.PostgreSQL91Dialect");        
+//        hibernateProperties.setProperty(
+//                "hibernate.dialect", "org.hibernate.dialect.OracleDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
         return hibernateProperties;
     }
-    
-    
-    
 }
